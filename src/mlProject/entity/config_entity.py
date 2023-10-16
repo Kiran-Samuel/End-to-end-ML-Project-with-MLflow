f2b4c98cf61de050cba6bca470c40acfeb093869
Kiran <kirankoshy41@gmail.com>
2023-10-16 22:34:42 +0400

    Data Transformation

--- src/mlProject/entity/config_entity.py
+++ src/mlProject/entity/config_entity.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 from pathlib import Path
 
-
 @dataclass(frozen=True)
 class DataIngestionConfig:
     root_dir: Path
@@ -15,3 +14,8 @@ class DataValidationConfig:
     STATUS_FILE: str
     unzip_data_dir: Path
     all_schema: dict
+
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
